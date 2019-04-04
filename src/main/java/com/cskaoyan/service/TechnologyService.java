118930a28435107c28bafbfe28c6e5efab01f64b
JacksonClaire <clairewangxueyang@163.com>
2019-04-04 09:15:33 +0800

    增加service层的示例

---
+++ src/main/java/com/cskaoyan/service/TechnologyService.java
@@ -0,0 +1,9 @@
+package com.cskaoyan.service;
+
+/**
+ * @author Wang Xueyang
+ * @create 2019-04-04
+ */
+public interface TechnologyService {
+
+}
---
+++ src/main/java/com/cskaoyan/service/impl/TechnologyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cskaoyan.service.impl;
+
+/**
+ * @author Wang Xueyang
+ * @create 2019-04-04
+ */
+public class TechnologyServiceImpl {
+}
